52e4d8d28b19f9191d0a922d6a8e90654a4b1919
Nollius <arquivosrafael21@gmail.com>
2025-11-28 11:49:28 -0300

    Alteracao do main.ts e inicio do auth.ts

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class Auth {
-  
+  private apiurl = "https://127.0.0:8000/api";
+  private tokenkey = "auth-token";
+  constructor(private http : HttpClient) {};
 }
