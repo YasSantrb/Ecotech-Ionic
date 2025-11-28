5348b93fa394452ca4e68145705d80fcfd5167f6
Nollius <arquivosrafael21@gmail.com>
2025-11-28 13:01:15 -0300

    atualizando login.page.ts

--- src/app/paginas/login/login.page.ts
+++ src/app/paginas/login/login.page.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { Auth } from 'src/app/services/auth';
 
 
 @Component({
@@ -10,27 +11,42 @@ import { Router } from '@angular/router';
   standalone: true,
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
-  imports: [CommonModule, FormsModule, IonicModule],
+  imports: [CommonModule, FormsModule, IonicModule, Auth],
 })
 export class LoginPage {
   username: string = '';
   password: string = '';
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private auth: Auth) {}
 
   FazerLogin() {
     if (!this.username || !this.password) {
       alert('Preencha todos os campos!');
       return;
     }
-
+    this.auth.login({ 
+      email: this.username, 
+      password: this.password })
+      .subscribe({
+      next: (res) => {
+      console.log('Logado!', res);
+      this.router.navigate(['/home']);
+    },
+    error: (err) => {
+      console.log('Deu ruim', err);
+      alert('Credenciais Inválidas')
+    },
+  });
+  
+  
     console.log('Usuário:', this.username);
     console.log('Senha:', this.password);
 
-    alert('Login realizado! (API será integrada depois)');
-  }
-
+}
   IrParaCadastro() {
     this.router.navigate(['/cadastro']);
   }
 }
+
