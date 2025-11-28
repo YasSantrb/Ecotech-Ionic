e2a0aff42d7a314b007740b238460d6fb441a4e0
Nollius <arquivosrafael21@gmail.com>
2025-11-28 12:13:22 -0300

    atualização 0.1 auth.ts

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -11,4 +11,26 @@ export class Auth {
   private apiurl = "https://127.0.0:8000/api";
   private tokenkey = "auth-token";
   constructor(private http : HttpClient) {};
+
+cadastro(data: any): Observable<any>{
+  return this.http.post('${this.apiurl}/cadastrar/', data);
+}
+login(credentials: any): Observable<any>{
+  return this.http.post('${this.apiurl}/login', credentials).pipe(
+    tap((res:any) => {
+    if(res.token){
+      localStorage.setItem(this.tokenkey,res.token)
+      }
+    })
+  )
+};
+gettoken(): string| null {
+  return localStorage.getItem(this.tokenkey);
+}
+logout(): void{
+  localStorage.removeItem(this.tokenkey);
+}
+isLoggedIn(): boolean {
+  return !!this.gettoken();
+}
 }
