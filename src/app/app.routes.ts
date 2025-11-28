a7b36468a27d307f96f967fdd8b312e54e317870
Guilherme <guilhermereisssf7@gmail.com>
2025-11-28 10:38:02 -0300

    Apaguei os tabs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,13 @@ export const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+  },  {
+    path: 'cadastro',
+    loadComponent: () => import('./paginas/cadastro/cadastro.page').then( m => m.CadastroPage)
   },
+  {
+    path: 'login',
+    loadComponent: () => import('./paginas/login/login.page').then( m => m.LoginPage)
+  },
+
 ];
