92615a7695facb9182e0fedaf5cc923c4f9f4dd4
Nollius <arquivosrafael21@gmail.com>
2025-12-01 10:48:27 -0300

    Feature: listar pontos de coleta

--- src/app/paginas/pontos-coleta/pontos-coleta.page.ts
+++ src/app/paginas/pontos-coleta/pontos-coleta.page.ts
@@ -4,7 +4,8 @@ import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { PontosService } from '../../services/pontos.service';
-
+import { Auth } from 'src/app/services/auth';
+import { Router } from '@angular/router';
 @Component({
   standalone: true,
   selector: 'app-pontos-coleta',
@@ -13,19 +14,56 @@ import { PontosService } from '../../services/pontos.service';
   styleUrls: ['./pontos-coleta.page.scss']
 })
 export class PontosColetaPage implements OnInit {
-
   pontos: any[] = [];
+  rua: string = '';
+  bairro: string = '';
+  cep: string = '';
+  numero: string = '';
+  telefone: string = '';
+  horario_funcionamento: string = '';
+  
+ngOnInit(): void {
+  this.Get_pontocoleta();
+}
 
-  constructor(private pontosService: PontosService) {}
+  constructor(
+    private router: Router,
+    private pontosService: PontosService, 
+    private auth: Auth) {}
 
-  ngOnInit() {
-    this.pontosService.listarPontos().subscribe({
-      next: (dados: any) => this.pontos = dados,
-      error: (err: any) => console.error(err)
+
+ Get_pontocoleta() {
+    this.pontosService.ListarPontos().subscribe({
+      next: (res) => {
+        this.pontos = res;
+        console.log("Pontos carregados:", res);
+      },
+      error: (err) => console.error("Erro ao buscar pontos:", err)
     });
   }
 
-  verDetalhes(id: number) {
-    // lógica futura
-  }
+  Criar_pontocoleta() {
+    if (!this.rua || !this.bairro || !this.cep || !this.numero || !this.telefone || !this.horario_funcionamento) {
+      alert('Preencha todos os campos!');
+      return;
+    }
+    this.pontosService.CriarPonto({
+      rua: this.rua,
+      bairro: this.bairro,
+      cep: this.cep,
+      numero: this.numero,
+      telefone: this.telefone,
+      horario_funcionamento: this.horario_funcionamento
+    })
+    .subscribe({
+      next: (res) => {
+        console.log('Ponto de coleta criado!', res);  
+        this.router.navigate(['/pontos-coleta']);
+      },
+      error: (err) => {
+        console.log('Erro ao criar ponto de coleta', err);
+        alert('Erro ao criar ponto de coleta. Código do erro: ' + err.status);
+      }
+    });
+}
 }
--- src/app/services/pontos.service.ts
+++ src/app/services/pontos.service.ts
@@ -1,16 +1,42 @@
 // src/app/services/pontos.service.ts
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Auth } from './auth';
 
 @Injectable({ providedIn: 'root' })
 export class PontosService {
+  private apiUrl = "https://yassant2.pythonanywhere.com/api/pontos_coleta/";
 
-  private apiUrl = "https://yassant2.pythonanywhere.com/api/";
+  constructor(
+    private http: HttpClient,
+    private auth: Auth
+  ) {}
 
-  constructor(private http: HttpClient) {}
-
-  listarPontos(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl);
+  getHeader() {
+    const token = this.auth.gettoken();
+    return {
+      headers: new HttpHeaders({
+        'Authorization': `Token ${token}`,
+        'content-type': 'application/json'
+      })
+    };
   }
+
+  ListarPontos(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl, this.getHeader());
+  }
+  CriarPonto(data: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, data, this.getHeader());
+  }
+  AtualizarPonto(id: number, data: any): Observable<any> {
+    return this.http.patch<any>(`${this.apiUrl}${id}/`, data, this.getHeader());
+  }
+  DeletarPonto(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}${id}/`, this.getHeader());
+  }
+
+
+
+
 }
